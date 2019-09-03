d431bbe13c647278d1fd390f435683ad6a6c5661
Julien Jorge <julien.jorge@stuff-o-matic.com>
2019-09-03 13:56:29 +0200

    Back to code: go to furthest feasible.

--- back-to-the-code/main.cpp
+++ back-to-the-code/main.cpp
@@ -1,46 +1,121 @@
+#include <array>
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 #include <random>
 
+constexpr int max_player_count = 4;
+constexpr int rows = 20;
+constexpr int columns = 35;
+constexpr char free_cell = '.';
+constexpr int max_int = std::numeric_limits< int >::max();
+
+struct point
+{
+  int x;
+  int y;
+
+  bool operator==( const point& p ) const
+  {
+    return ( x == p.x ) && ( y == p.y );
+  }
+};
+
+struct goal
+{
+  point p;
+  int distance;
+};
+  
+typedef std::array< std::array< char, columns >, rows > terrain;
+
+goal furthest_reachable_free_cell
+( const terrain& terrain, const std::array< point, max_player_count >& players,
+  int player_count )
+{
+  goal result{ point{ -1, -1 }, -1 };
+  
+  for ( int y( 0 ); y != rows; ++y )
+    for ( int x( 0 ); x != columns; ++x )
+      if ( terrain[ y ][ x ] == free_cell )
+      {
+        std::array< int, max_player_count > distance_to_player
+          {
+           max_int, max_int, max_int, max_int
+          };
+
+        for ( int i( 0 ); i != player_count; ++i )
+          {
+            const point& p( players[ i ] );
+            distance_to_player[ i ] =
+              std::abs( x - p.x ) + std::abs( y - p.y );
+          }
+
+        const int distance_to_player_0( distance_to_player[ 0 ] );
+        
+        if ( distance_to_player_0 > result.distance )
+          {
+            if ( ( distance_to_player_0 < distance_to_player[ 1 ] )
+                 && ( distance_to_player_0 < distance_to_player[ 2 ] )
+                 && ( distance_to_player_0 < distance_to_player[ 3 ] ) )
+              {
+                result.distance = distance_to_player_0;
+                result.p.x = x;
+                result.p.y = y;
+              }
+          }
+      }
+
+  return result;
+}
+
 int main()
 {
-  std::random_device rd;
-  std::mt19937 random(rd());
-  std::uniform_int_distribution<> random_row( 0, 19 );
-  std::uniform_int_distribution<> random_column( 0, 34 );
+  int player_count;
+  std::cin >> player_count;
+  ++player_count;
   
-  int opponent_count;
-  std::cin >> opponent_count;
+  std::array< point, max_player_count > players =
+    {
+     point{ max_int, max_int },
+     point{ max_int, max_int },
+     point{ max_int, max_int },
+     point{ max_int, max_int }
+    };
 
+  goal current_goal{ point{ -1, -1 }, -1 };
+  
   while (1)
     {
       int round;
-      int my_x;
-      int my_y;
-      int my_rewind;
         
-      std::cin >> round >> my_x >> my_y >> my_rewind;
+      std::cin >> round;
         
-      for (int i( 0 ); i != opponent_count; ++i)
+      for (int i( 0 ); i != player_count; ++i)
         {
-          int opponent_x;
-          int opponent_y;
           int opponent_rewind;
-          std::cin >> opponent_x >> opponent_y >> opponent_rewind;
+          std::cin >> players[ i ].x >> players[ i ].y >> opponent_rewind;
         }
 
       std::cin.ignore();
-        
-      for (int i( 0 ); i != 20; ++i)
-        {
-          std::string line;
-          std::cin >> line;
-          std::cin.ignore();
-        }
 
-      std::cout << random_column(random) << ' ' << random_row(random) << '\n';
+      terrain terrain;
+      
+      for ( int i( 0 ); i != rows; ++i )
+        std::cin.read( terrain[ i ].data(), columns ).ignore();
+
+      const goal new_goal
+        ( furthest_reachable_free_cell( terrain, players, player_count ) );
+
+      if ( ( current_goal.p == players[ 0 ] )
+           || ( new_goal.distance > current_goal.distance ) )
+        current_goal = new_goal;
+
+      if ( current_goal.p.x < 0 )
+        std::cout << players[ 0 ].x << ' ' << players[ 0 ].y << '\n';
+      else
+        std::cout << current_goal.p.x << ' ' << current_goal.p.y << '\n';
     }
 
   return 0;
